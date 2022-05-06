8ea9114d7738f432db76b06f4fa898896d9b64c4
estebanheish <estebanheish@gmail.com>
2022-05-06 20:38:29 +0200

    a

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -55,6 +55,32 @@ in
         crates-nvim
       ];
 
+      extraConfig = ''
+        lua << EOF
+        require('settings')
+        require('maps')
+        require('lsp')
+        require('lsp_saga')
+        require('treesitter')
+        require('crates').setup()
+        require('colorizer').setup()
+        
+        require('neorg').setup {
+          load = {
+              ["core.defaults"] = {}
+          }
+        }
+        
+        -- colors
+        require('base16-colorscheme').setup({
+        base00 = '#212121', base01 = '#303030', base02 = '#353535', base03 = '#4A4A4A',
+        base04 = '#B2CCD6', base05 = '#EEFFFF', base06 = '#EEFFFF', base07 = '#FFFFFF',
+        base08 = '#F07178', base09 = '#F78C6C', base0A = '#FFCB6B', base0B = '#C3E88D',
+        base0C = '#89DDFF', base0D = '#82AAFF', base0E = '#C792EA', base0F = '#FF5370',
+        })
+        EOF
+      '';
+
       extraPackages = [
       ];
 
