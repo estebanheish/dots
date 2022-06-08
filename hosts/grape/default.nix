757956e116c18d91d5b80dd07de7cbebbf3ff709
estebanheish <estebanheish@gmail.com>
2022-06-08 17:18:01 +0200

    syncthing

--- hosts/grape/default.nix
+++ hosts/grape/default.nix
@@ -12,10 +12,24 @@
   systemd.network.enable = true;
   systemd.network.wait-online.anyInterface = true;
 
+  # external drive
+  fileSystems."/home/${config.user.name}/data" = { 
+    device = "/dev/disk/by-label/data";
+    fsType = "btrfs";
+  };
+
   # services
   services.openssh = {
     enable = true;
     passwordAuthentication = true;
+  };
+
+  services = {
+    syncthing = {
+      enable = true;
+      user = "heis";
+      dataDir = "/home/${config.user.name}/data";    # Default folder for new synced folders
+    };
   };
 
 }
