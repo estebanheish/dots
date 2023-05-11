c82e798e0e531f5f76f61ebb518332b92966b921
estebanheish <estebanheish@gmail.com>
2023-05-11 19:03:30 +0200

    dlna module

---
+++ modules/nixos/dlna/default.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  services.minidlna = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      friendly_name = "SHH DLNA SERVER";
+      media_dir = ["/mnt/media/"];
+      inotify = "yes";
+    };
+  };
+  users.users."minidlna" = {
+    extraGroups = [ "users" ]; # so minidlna can access the files.
+  };
+  environment.systemPackages = [ pkgs.inotify-tools ];
+}
+
