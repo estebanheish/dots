f1330ef4a334ff8112c67818d030c41893a2e60a
estebanheish <estebanheish@gmail.com>
2023-10-21 04:41:48 +0200

    ssh hosts ip fix

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -3,11 +3,11 @@
     enable = true;
     matchBlocks = {
       "grape" = {
-        hostname = "192.168.18.2";
+        hostname = "192.168.18.10";
         user = "heis";
       };
       "nyx" = {
-        hostname = "192.168.18.10";
+        hostname = "192.168.18.2";
         user = "heis";
       };
     };
