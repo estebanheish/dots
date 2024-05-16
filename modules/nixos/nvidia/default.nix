552754b3319e27504b476f27cb0c81cfa06255e3
estebanheish <estebanheish@gmail.com>
2024-05-16 15:20:24 +0200

    lock

--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -27,7 +27,7 @@
   environment.sessionVariables = {
     GBM_BACKEND = "nvidia-drm";
     LIBVA_DRIVER_NAME = "nvidia";
-    WLR_NO_HARDWARE_CURSORS = "1"; # hyprland
+    # WLR_NO_HARDWARE_CURSORS = "1"; # hyprland (it works from 0.40)
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     __GL_GSYNC_ALLOWED = "1";
   };
