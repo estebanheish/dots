5bf0b59b86edb5190d24d6936c6d92970fac28d0
estebanheish <estebanheish@gmail.com>
2024-07-11 18:17:19 +0200

    mnt

--- modules/nixos/hostsfile/default.nix
+++ modules/nixos/hostsfile/default.nix
@@ -8,7 +8,7 @@
       192.168.18.4 rivet
       0.0.0.0 www.youtube.com
       0.0.0.0 www.twitch.tv
-      0.0.0.0 www.amazon.tv
+      0.0.0.0 www.amazon.es
       0.0.0.0 www.ebay.com
       0.0.0.0 www.pccomponentes.com
     '';
