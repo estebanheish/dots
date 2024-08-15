d51c51279464c200a26f31de9be324ed836c55d6
estebanheish <estebanheish@gmail.com>
2024-08-15 17:44:33 +0200

    zellij: theme

--- modules/home-manager/zellij/default.nix
+++ modules/home-manager/zellij/default.nix
@@ -4,7 +4,20 @@
     settings = {
       simplified_ui = true;
       pane_frames = false;
-      # theme = "nord";
+      theme = "rose-pine";
+      themes.rose-pine = {
+        bg = "#403d52";
+        fg = "#e0def4";
+        red = "#eb6f92";
+        green = "#31748f";
+        blue = "#9ccfd8";
+        yellow = "#f6c177";
+        magenta = "#c4a7e7";
+        orange = "#fe640b";
+        cyan = "#ebbcba";
+        black = "#26233a";
+        white = "#e0def4";
+      };
       default_layout = "compact";
       copy_command = "wl-copy";
       keybinds.tab = {
