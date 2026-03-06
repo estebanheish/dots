8a3af4eb5406996c3d4f75447c8670b19c4c9f72
estebanheish <estebanheish@gmail.com>
2026-03-06 12:37:45 +0100

    add some browser bookmarks

--- modules/home-manager/librewolf/default.nix
+++ modules/home-manager/librewolf/default.nix
@@ -10,5 +10,25 @@
       "general.autoScroll" = true;
       # "media.autoplay.default" = "0";
     };
+    profiles.default.bookmarks = {
+      force = true;
+      settings = [
+        {
+          name = "Syncthing";
+          url = "http://localhost:8384/";
+          keyword = "syncthing";
+        }
+        {
+          name = "Audiobookshelf";
+          url = "http://localhost:1778/";
+          keyword = "audiobookshelf";
+        }
+        {
+          name = "Llamaswap";
+          url = "http://localhost:11344/";
+          keyword = "llamaswap";
+        }
+      ];
+    };
   };
 }
